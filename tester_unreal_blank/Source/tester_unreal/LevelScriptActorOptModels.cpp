8b316d0cceef44a9ec45851b80c95b9ef3c94183
wanxwang <wanxin-wang@uiowa.edu>
2022-01-26 11:56:04 -0600

    spawn BP actors in code to avoid messy BluePrint

--- tester_unreal_blank/Source/tester_unreal/LevelScriptActorOptModels.cpp
+++ tester_unreal_blank/Source/tester_unreal/LevelScriptActorOptModels.cpp
@@ -40,3 +40,27 @@ void ALevelScriptActorOptModels::GetModelTM0(ModelType type, FTransform& tm_driv
 
 
 }
+
+void ALevelScriptActorOptModels::SpawnPairs(const TArray<UClass*>& templates_drivee, const TArray<UClass*>& templates_driver)
+{
+	ModelType type = GetModelType();
+	FTransform tm_driver, tm_drivee;
+	GetModelTM0(type, tm_driver, tm_drivee);
+	UWorld* world = GetWorld();
+	FActorSpawnParameters Params;
+	Params.bNoFail = true;
+	Params.bDeferConstruction = true; // We defer construction so that we set ParentComponentActor prior to component registration so they appear selected
+	Params.bAllowDuringConstructionScript = true;
+	Params.OverrideLevel = this->GetLevel();
+	Params.Name = "drivee";
+	if (!HasAllFlags(RF_Transactional))
+	{
+		Params.ObjectFlags &= ~RF_Transactional;
+	}
+	AActor* actor_drivee = world->SpawnActor(templates_drivee[type], &tm_drivee, Params);
+	check(NULL != actor_drivee);
+	
+	Params.Name = "driver";
+	AActor* actor_driver = world->SpawnActor(templates_driver[type], &tm_driver, Params);
+	check(NULL != actor_driver);
+}
--- tester_unreal_blank/Source/tester_unreal/LevelScriptActorOptModels.h
+++ tester_unreal_blank/Source/tester_unreal/LevelScriptActorOptModels.h
@@ -11,7 +11,7 @@ enum ModelType
 {
 	ManMakeHuman163 = 0	UMETA(DisplayName = "MakeHuman163"),
 	WomanMixamo65		UMETA(DisplayName = "Mixamo65"),
-	ChildJasper67 			UMETA(DisplayName = "Jasper67"),
+	ChildJasper67 		UMETA(DisplayName = "Jasper67"),
 	Child
 };
 
@@ -22,10 +22,11 @@ UCLASS()
 class TESTER_UNREAL_API ALevelScriptActorOptModels : public ALevelScriptActor
 {
 	GENERATED_BODY()
-public:
-	UFUNCTION(BlueprintCallable, Category = Settings, meta=(PinShownByDefault))
+private:
 	ModelType GetModelType();
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Settings, meta=(PinShownByDefault))
 	void GetModelTM0(ModelType type, FTransform& tm_driver, FTransform& tm_drivee);
+public:
+	UFUNCTION(BlueprintCallable, Category = Settings, meta = (PinShownByDefault))
+	void SpawnPairs(const TArray<UClass*>& templates_drivee, const TArray<UClass*>& templates_driver);
+
 };
